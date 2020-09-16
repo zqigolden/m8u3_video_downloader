35385fc710d0e55b8695031011e0362ea6a107ba
qzhu <qzhu@aibee.com>
2020-09-16 13:48:10 +0800

    update

--- run_download_pangzitv.sh
+++ run_download_pangzitv.sh
@@ -1,13 +1,14 @@
 link=$1
 
+cd src
+
 #get name
 name=$(curl -s "$link" | grep mac_name | sed -E "s/.*mac_name=\'([^\' ]*).*/\1/g")
-echo $link $name
 
-mkdir $name
-cd src
+test -d $name || mkdir $name
+echo $link $name | tee -a log
 
 ../src/get_m3u8_links.sh $link $name
 ../src/start_download.sh $link
 
-mv *csv *mp4 *log ../$name
+mv *csv *log ../$name
--- src/start_download.sh
+++ src/start_download.sh
@@ -1,1 +1,1 @@
-cat all_videos_cvt.csv | xargs -P 1 -n2 ./download_m3u8.sh | tee log
+(cat all_videos_cvt.csv | xargs -n2 ./download_m3u8.sh) 2>&1 | tee -a log
