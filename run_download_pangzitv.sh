3c715afccc26e7a0310e47ddc128a0c1db05a0f2
qzhu <qzhu@aibee.com>
2020-09-18 14:37:25 +0800

    update: load page from detail page

--- run_download_pangzitv.sh
+++ run_download_pangzitv.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 link=$1
 
 cd src
+
+if [[ $link~=*vod-detail-id* ]]; then
+    old_link=link
+    link=$(sed -E 's/vod-detail-id-([0-9]+).*/vod-play-id-\1-src-1-num-1.html/g' <<< $link)
+    echo "Redirect to $link"
+fi
 
 #get name
 name=$(curl -s "$link" | grep mac_name | sed -E "s/.*mac_name=\'([^\' ]*).*/\1/g")
